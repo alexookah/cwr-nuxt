cc49a9da884b27b2a691f9c02bad7ccae5d2d6ea
Alex Lykesas <alexookah@me.com>
2024-01-08 17:59:10 +0200

    Add overrides

---
+++ components/generalElements/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="sticky top-0 z-40 bg-white shadow-sm shadow-light-500">
+      <div class="container flex items-center justify-between py-4">
+        <div class="flex items-center">
+          <MenuTrigger class="lg:hidden" />
+          <Logo class="md:w-[160px]" />
+        </div>
+        <MainMenu class="items-center hidden gap-6 text-sm text-gray-500 lg:flex lg:px-4" />
+        <div class="flex gap-4 justify-end items-center md:w-[160px] flex-1 ml-auto">
+          <NuxtLink to="/wishlist" title="Wishlist">
+            <Icon name="ion:heart-outline" size="20" />
+          </NuxtLink>
+          <SignInLink />
+          <CartTrigger />
+        </div>
+      </div>
+    </header>
+  </template>
---
+++ components/generalElements/MainMenu.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav>
+      <NuxtLink to="/products">{{ $t('messages.general.allProducts') }}</NuxtLink>
+      <NuxtLink to="/contact">{{ $t('messages.general.contact') }}</NuxtLink>
+  
+      <NuxtLink class="lg:hidden" to="/wishlist" :prefetch="false">Wishlist</NuxtLink>
+      <NuxtLink class="lg:hidden" to="/my-account" :prefetch="false">My Account</NuxtLink>
+    </nav>
+  </template>
