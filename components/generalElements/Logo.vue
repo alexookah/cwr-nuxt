f4b54ca6bcab8196c8c13b06b007c8a3c56cb62c
Alex Lykesas <alexookah@me.com>
2024-01-08 17:59:10 +0200

    Add overrides

---
+++ components/generalElements/Logo.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+const logoUrl = runtimeConfig?.public?.LOGO || null;
+</script>
+
+<template>
+  <NuxtLink to="/">
+    <img v-if="logoUrl" :src="logoUrl" alt="Logo" class="object-contain h-10" />
+    <div v-else class="flex items-center gap-2 text-lg font-bold">
+      <NuxtImg
+        :width="50"
+        :height="50"
+        src="../../cwr_small.png"
+        fit="outside"
+        format="webp"
+        densities="x1 x2" />
+
+    </div>
+  </NuxtLink>
+</template>
