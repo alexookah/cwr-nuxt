c2c0ecb8867bd31cd1aee558ce32ab3de817d2e7
Alex Lykesas <alexookah@me.com>
2024-01-29 13:20:24 +0200

    Replace NuxtImage with NuxtPicture

--- components/Events.vue
+++ components/Events.vue
@@ -7,8 +7,8 @@ const events = await queryContent('events').sort({ releaseDate: -1 }).find();
     <div class="grid grid-cols-4 gap-4">
         <div v-for="event in events" :key="event.slug" class="relative overflow-hidden">
             <router-link :to="`/albums/${event.slug}`">
-                <NuxtImg class="w-full h-auto pb-5 block transform transition-transform duration-300 hover:scale-105"
-                :src="event.image.src" :alt="event.image.alt" format="webp" />
+                <NuxtPicture class="w-full h-auto pb-5 block transform transition-transform duration-300 hover:scale-105"
+                :src="event.image.src" :alt="event.image.alt" format="avif,webp" loading="lazy" />
             </router-link>
         </div>
     </div>
--- components/Photos.vue
+++ components/Photos.vue
@@ -6,8 +6,8 @@ const photos = await queryContent('photos').findOne();
     <h2 class="text-center text-5xl mt-20 mb-10">Photos</h2>
     <div class="grid grid-cols-4 gap-4">
         <div v-for="photo in photos.body" :key="photo.src" class="relative overflow-hidden">
-            <NuxtImg class="w-full h-auto pb-5 block transform transition-transform duration-300 hover:scale-105"
-                :src="`/${photo.src}`" :alt="photo.alt" format="webp" />
+            <NuxtPicture class="w-full h-auto pb-5 block transform transition-transform duration-300 hover:scale-105"
+                :src="`/${photo.src}`" :alt="photo.alt" format="avif,webp" loading="lazy" />
         </div>
     </div>
 </template>
