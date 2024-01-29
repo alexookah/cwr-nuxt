8d079a75a54b523c06797261e73930dac1b0b227
Alex Lykesas <alexookah@me.com>
2024-01-29 12:59:04 +0200

    Update photos.keys

--- components/Photos.vue
+++ components/Photos.vue
@@ -6,7 +6,7 @@ console.log(photos.body);
 <template>
     <h2 class="text-center text-5xl mt-20 mb-10">Photos</h2>
     <div class="grid grid-cols-4 gap-4">
-        <div v-for="photo in photos.body" :key="photo.image.src" class="relative overflow-hidden">
+        <div v-for="photo in photos.body" :key="photo.src" class="relative overflow-hidden">
             <NuxtImg class="w-full h-auto pb-5 block transform transition-transform duration-300 hover:scale-105"
                 :src="`/${photo.src}`" :alt="photo.alt" format="webp" />
         </div>
