d0f8ce852b357c7722123f3236617506a5a7fa97
Alex Lykesas <alexookah@me.com>
2024-01-31 00:18:40 +0200

    Minor updates

--- pages/event/[event].vue
+++ pages/event/[event].vue
@@ -11,15 +11,15 @@ const openImageInLightbox = (src: string) => {
 
 
 <template>
-    <div class="event-container mx-auto max-w-4xl p-6 my-14 bg-white shadow-lg rounded-lg min-w-[800px]">
+    <div class="container mx-auto p-6 my-14 max-w-4xl bg-white shadow-lg rounded-lg min-w-[800px]">
         <h1 class="text-4xl font-bold text-center mb-6 text-gray-800">{{ event.name }}</h1>
 
-        <div class="image-container mb-6 flex justify-center">
+        <div class="mb-6 flex justify-center">
             <NuxtImg :src="event.image.src" :alt="event.image.alt" class="w-full max-w-[300px] h-auto rounded-md shadow-sm cursor-pointer"
                 @click="openImageInLightbox(event.image.src)" />
         </div>
 
-        <div class="event-details mb-6">
+        <div class="mb-6">
             <h2 class="text-2xl font-semibold mb-4 text-gray-700">Event Details</h2>
             <p class="mb-2 text-gray-600"><strong>Date:</strong> {{ event.date }}</p>
             <p class="mb-2 text-gray-600"><strong>Time:</strong> {{ event.time }}</p>
@@ -30,17 +30,17 @@ const openImageInLightbox = (src: string) => {
             </p>
         </div>
 
-        <div v-if="event.body" class="event-info mb-4 p-4 bg-gray-100 rounded-lg">
+        <div v-if="event.body">
             <h2 class="text-xl font-semibold mb-2">Event Info</h2>
             <div class="event-body mb-6 prose max-w-none" v-html="event.body"></div>
         </div>
 
-        <div v-if="event.artists" class="artists mb-6">
+        <div v-if="event.artists" class="mb-6">
             <h2 class="text-2xl font-semibold mb-4 text-gray-700">Artists</h2>
             <p class="text-gray-600">{{ event.artists }}</p>
         </div>
 
-        <div class="event-links">
+        <div>
             <a :href="event.eventLink" target="_blank"
                 class="inline-block bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full mt-4">
                 Event Link
