83fdb63176d1d4bd5fe24717299f74eb9b0d9a2a
Alex Lykesas <alexookah@me.com>
2024-02-11 21:01:02 +0200

    Add h1 tag in home

--- components/generalElements/HeroBanner.vue
+++ components/generalElements/HeroBanner.vue
@@ -1,9 +1,10 @@
   
 <template>
-    <div class="container flex justify-center pt-10"> 
-        <NuxtImg src="/logo-black.svg" height="400" width="500" class="" alt="Come with Reverse logo" />
-    </div>
-    <!-- <swiper :modules="modules" :slides-per-view="1" :space-between="0" navigation :pagination="{ clickable: true }"
+  <div class="container flex flex-col justify-center pt-10 items-center">
+    <NuxtImg src="/logo-black.svg" height="400" width="500" class="" alt="Come with Reverse logo" />
+    <h1 class="text-4xl pt-6">Dark Side of the Pop</h1>
+  </div>
+  <!-- <swiper :modules="modules" :slides-per-view="1" :space-between="0" navigation :pagination="{ clickable: true }"
         :scrollbar="{ draggable: true }" @swiper="onSwiper" @slideChange="onSlideChange">
         <swiper-slide>
             <img src="/woonuxt_base/assets/images/hero-3.jpg" alt="Brand 1" />
@@ -37,28 +38,28 @@ import 'swiper/css/scrollbar';
 
 // Import Swiper styles
 export default {
-    components: {
-        Swiper,
-        SwiperSlide,
-    },
-    setup() {
-        const onSwiper = (swiper) => {
-            console.log(swiper);
-        };
-        const onSlideChange = () => {
-            console.log('slide change');
-        };
-        return {
-            onSwiper,
-            onSlideChange,
-            modules: [Navigation, Pagination, A11y],
-            loop: true,
-            speed: 500,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        };
-    },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log('slide change');
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+      modules: [Navigation, Pagination, A11y],
+      loop: true,
+      speed: 500,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    };
+  },
 };
 </script>
