936ef0345fff13c9a8014a7da8edd0150a2f3e23
Alex Lykesas <alexookah@me.com>
2024-02-13 00:19:41 +0200

    Specify width&height

--- components/Events.vue
+++ components/Events.vue
@@ -9,7 +9,7 @@ const events = await queryContent('events').sort({ date: -1 }).find();
         <div v-for="event in events" :key="event.slug" class="relative overflow-hidden">
             <NuxtLink :to="`/event/${formatURI(event.slug)}`" :title="event.name">
                 <NuxtImg class="w-full h-auto pb-5 block transform transition-transform duration-300 hover:scale-105"
-                :src="event.image.src" :alt="event.image.alt" format="avif,webp" loading="lazy" />
+                :src="event.image.src" :alt="event.image.alt" format="avif,webp" loading="lazy" width="638" height="900"  />
             </NuxtLink>
         </div>
     </div>
