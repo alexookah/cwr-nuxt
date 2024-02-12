f78d0c7b1352a4aca6ef4a9eea2b663c37cb7c6a
Alex Lykesas <alexookah@me.com>
2024-02-12 18:18:38 +0200

    Some further fixes

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <h2 class="text-4xl text-center mt-20 mb-10">About Us</h2>
-    <div class="max-w-4xl mx-auto p-5">
+    <div class="max-w-4xl mx-auto p-5 text-justify">
       <NuxtImg src="/cwr_members.jpg" class="pb-5 object-contain w-full" alt="Come with Reverse - about us" format="avif,webp" />
       <p class="text-base text-gray-700 mb-6">
         <span class="font-semibold">Come With Reverse</span> is a band formed in 2010 in Thessaloniki, Greece. Our band consists of Petros Leivadas (vocals - guitars), Alexander
-        Lykesas (drums), Alexander Passalides (keyboard), and Bob Architektonidis (management). Influenced by a broad spectrum of music genres, our members have a background in
+        Lykesas (drums), Alexander Passalides (keyboard), and Bob Architektonidis (manager). Influenced by a broad spectrum of music genres, our members have a background in
         classical music, extending to traditional-ethnic music from various countries.
       </p>
       <p class="text-base text-gray-700 mb-6">
