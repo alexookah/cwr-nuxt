9363716c0e22fa7bbbef49c4065d03ec356db0dd
Alex Lykesas <alexookah@me.com>
2024-03-01 17:52:12 +0200

    Remove redirects

--- middleware/redirects.ts
+++
@@ -1,7 +0,0 @@
-export default definePageMiddleware((to, from) => {
-    if (to.path === '/shop') {
-      return navigateTo('/products');
-    } else if (to.path === '/contact-us') {
-      return navigateTo('/contact');
-    }
-  });
