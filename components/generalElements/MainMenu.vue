2127d9e68a282dcff02c09470dc95f61d7107d0f
Alex Lykesas <alexookah@me.com>
2024-06-21 23:26:30 +0300

    Test

--- components/generalElements/MainMenu.vue
+++ components/generalElements/MainMenu.vue
@@ -5,6 +5,6 @@
       <NuxtLink to="/contact">{{ $t('messages.general.contact') }}</NuxtLink>
   
       <NuxtLink class="lg:hidden" to="/wishlist" :prefetch="false">Wishlist</NuxtLink>
-      <NuxtLink class="lg:hidden" to="/my-account" :prefetch="false">My Account</NuxtLink>
+      <NuxtLink class="lg:hidden" to="/my-account" :prefetch="false">My Accounts/NuxtLink>
     </nav>
   </template>
