5191f925fb418c8ae1f3a7d8601a982cd1fd1799
Alex Lykesas <alexookah@me.com>
2024-07-18 02:53:34 +0300

    Update Header

--- components/generalElements/AppHeader.vue
+++ components/generalElements/AppHeader.vue
@@ -5,13 +5,14 @@
         <!-- <MenuTrigger class="lg:hidden" /> -->
         <Logo />
       </div>
-      <!-- <MainMenu class="items-center hidden gap-10 text-sm text-gray-500 lg:flex lg:px-4" /> -->
-      <NuxtLink to="/products" class="flex items-center justify-center bg-gray-50 text-gray-600 px-4 py-2 rounded-full ml-4 hover:bg-gray-100">
+      <MainMenu class="items-center hidden gap-10 text-sm text-gray-500 lg:flex lg:px-4" />
+
+
+      <div class="flex gap-8 justify-end items-center">
+        <NuxtLink to="/products" class="lg:hidden flex items-center justify-center bg-gray-50 text-gray-600 px-4 py-2 rounded-full ml-4 hover:bg-gray-100">
         <Icon name="ion:bag-outline" size="21" class="mr-2" />
         <span>Shop</span>
       </NuxtLink>
-
-      <div class="flex gap-6 justify-end items-center">
         <NuxtLink to="/my-account" class="sm:hidden">
           <Icon name="ion:person-outline" size="21" class="mr-1 md:mr-0 mt-1" />
         </NuxtLink>
