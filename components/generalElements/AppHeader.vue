08dc7d27154bb22e0afb07e14b046041541a8f21
Alex Lykesas <alexookah@me.com>
2024-07-18 16:43:30 +0300

    Fix header

--- components/generalElements/AppHeader.vue
+++ components/generalElements/AppHeader.vue
@@ -7,12 +7,11 @@
       </div>
       <MainMenu class="items-center hidden gap-10 text-sm text-gray-500 lg:flex lg:px-4" />
 
-
       <div class="flex gap-8 justify-end items-center">
         <NuxtLink to="/products" class="lg:hidden flex items-center justify-center bg-gray-100 text-gray-600 px-4 py-2 rounded-full ml-4 hover:bg-gray-200">
-        <Icon name="ion:bag-outline" size="21" class="mr-2" />
-        <span>Shop</span>
-      </NuxtLink>
+          <Icon name="ion:bag-outline" size="21" class="mr-2" />
+          <span>Shop</span>
+        </NuxtLink>
         <NuxtLink to="/my-account" class="sm:hidden">
           <Icon name="ion:person-outline" size="21" class="mr-1 md:mr-0 mt-1" />
         </NuxtLink>
--- components/generalElements/MainMenu.vue
+++ components/generalElements/MainMenu.vue
@@ -1,10 +1,15 @@
 <template>
-    <nav>
-      <NuxtLink to="/">{{ $t('messages.general.home') }}</NuxtLink>
-      <NuxtLink to="/products"> Shop </NuxtLink>
-      <NuxtLink to="/contact">{{ $t('messages.general.contact') }}</NuxtLink>
-  
-      <NuxtLink class="lg:hidden" to="/wishlist" :prefetch="false">Wishlist</NuxtLink>
-      <NuxtLink class="lg:hidden" to="/my-account" :prefetch="false">My Account</NuxtLink>
-    </nav>
-  </template>
+  <nav>
+    <NuxtLink to="/">{{ $t('messages.general.home') }}</NuxtLink>
+    <!-- <NuxtLink to="/products"> Shop </NuxtLink> -->
+    <NuxtLink to="/products" class=" flex items-center justify-center bg-gray-200 text-gray-600 px-4 py-2 rounded-full ml-4 hover:bg-gray-300">
+      <Icon name="ion:bag-outline" size="21" class="mr-2" />
+      <span>Shop</span>
+    </NuxtLink>
+
+    <NuxtLink to="/contact">{{ $t('messages.general.contact') }}</NuxtLink>
+
+    <NuxtLink class="lg:hidden" to="/wishlist" :prefetch="false">Wishlist</NuxtLink>
+    <NuxtLink class="lg:hidden" to="/my-account" :prefetch="false">My Account</NuxtLink>
+  </nav>
+</template>
